b928068cc6c17fe18925629cd8aa5979b05288a3
Bowuigi <bowuigi@vern.cc>
2025-11-03 01:15:05 -0300

    feat(backend): Session management API

---
+++ backend/lib/Session.php
@@ -0,0 +1,36 @@
+<?php
+require_once "/app/lib/Database.php";
+
+// Every method in this class performs side effects
+class Session {
+	public function __construct() {
+		ini_set('session.cookie_httponly', 1);
+		ini_set('session.use_strict_mode', 1);
+		ini_set('session.cookie_samesite', 'Lax');
+		// NOTE: Set to 1 on production to enable HTTPS-only authentication
+		ini_set('session.cookie_secure', 0);
+
+		session_start();
+	}
+
+	public function set(string $key, string|bool $value) {
+		$_SESSION[$key] = $value;
+	}
+
+	public function get(string $key) {
+		return $_SESSION[$key] ?? null;
+	}
+
+	public function login(string $username) {
+		session_regenerate_id(true);
+		$this->set('logged_in', true);
+		$this->set('username', $username);
+		// No need to verify the login server-side since the session files are already stored in the server
+	}
+
+	public function logout() {
+		session_unset();
+		session_destroy();
+	}
+}
+?>
